3bd0eda785dbf72e8bffc316364c29f5bab31b00
moonfall000 <senryckeyminecraft@gmail.com>
2026-08-10 12:56:38 +0800

    chore: revert to safe local image path

--- app/page.js
+++ app/page.js
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import { hero, about, project, contest, social, tabinfo, friends } from '../config';
 import { Analytics } from "@vercel/analytics/next"
 
+const avatarUrl = "/YueFon.png";
+
+
 export default function Home() {
   const categories = Object.keys(tabinfo);
   const [activeTab, setActiveTab] = useState(categories[0]);
